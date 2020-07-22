cdc355a545928b399540718f5bb7b458af716b72
Zachary Rankin <zach@rknmail.com>
2020-07-22 17:01:58 +0000

    Add some basic tests using QUnit
    Just experimenting with QUnit. It was easy to setup and get something
    going.

---
+++ js-keymaps/public/keymap_test.mjs
@@ -0,0 +1,35 @@
+import Keymap from './keymap.mjs';
+
+QUnit.test('should call bindKey when adding a keybinding', assert => {
+  assert.expect(2);
+
+  const keymap = new Keymap({
+    bindKey(key, callback) {
+      assert.equal(key, 'ctrl+s');
+      assert.equal(typeof callback, 'function');
+    }
+  });
+  keymap.add({
+    key: 'ctrl+s',
+    selector: 'body',
+    eventType: 'core:save',
+  });
+});
+
+QUnit.test('should return correct number of keybindings added', assert => {
+  const keymap = new Keymap({
+    bindKey(key, callback) {}
+  });
+  keymap.add({
+    key: 'ctrl+s',
+    selector: 'body',
+    eventType: 'core:save',
+  });
+  keymap.add({
+    key: 'esc',
+    selector: 'body',
+    eventType: 'core:exit',
+  });
+
+  assert.equal(keymap.getNumberOfKeybindings(), 2);
+});
